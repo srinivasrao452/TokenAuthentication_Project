dce6dbf8a0d14364852c3c51cbafb65b34a70974
srinivasrao452 <30620328+srinivasrao452@users.noreply.github.com>
2019-02-20 09:17:40 +0530

    Commit for Pro Serializer

--- TokenAuthentication_Project/TokenAuthentication_App/serializers.py
+++ TokenAuthentication_Project/TokenAuthentication_App/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Emp
 
+# This Userdefined Serializer Class
 class EmpSerializer(serializers.ModelSerializer):
     class Meta :
         model = Emp
