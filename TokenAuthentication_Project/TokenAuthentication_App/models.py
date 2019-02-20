04080966c2601efb526d1dc673ddb92b6e626ce5
srinivasrao452 <30620328+srinivasrao452@users.noreply.github.com>
2019-02-20 08:33:50 +0530

    Initial commit

---
+++ TokenAuthentication_Project/TokenAuthentication_App/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Emp(models.Model):
+    empid = models.IntegerField(primary_key=True)
+    empname = models.CharField(max_length=200)
+    empsal = models.DecimalField(max_digits=10 , decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
